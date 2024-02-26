015aa782b13a3cfc1d0e4e73db5e0037bd9fafc4
marinaescriva <marinaescriva.24@gamil.com>
2024-02-26 10:14:20 +0100

    refactor server.ts to app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config'  //es la solucion de arriba, también comenté la dotenv.config().
 import { app } from './app';
 
-
-
 const PORT = process.env.PORT || 4001; //si existe el puerto y sino coges el de env
 
 
